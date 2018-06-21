a1838f6105c33ce84bdc56259e4207b951cec78f
yuji <yuoda@yahoo.co.jp>
2018-06-21 21:18:11 +0900

    trivial change

--- src/main/java/my/formapp/RestPassController.java
+++ src/main/java/my/formapp/RestPassController.java
@@ -1,7 +1,6 @@
 package my.formapp;
 
 import org.springframework.http.MediaType;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
